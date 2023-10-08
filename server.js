0cce3a124bef1f96ac1159c9f87d6938dc9cfa65
Logan Gibler <logan@DESKTOP-8TT4RDK.attlocal.net>
2023-10-08 01:47:10 -0400

    -

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { JWT_SECRET = "neverTell" } = process.env;
 app.enable("trust proxy");
 app.use(morgan("dev"));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const env = "QA";
 
@@ -27,12 +27,12 @@ const User = require("./db/userModel");
 const Post = require("./db/postModel");
 const Feedback = require("./db/feedbackModel");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function (req, res) {
